f528fe45d54a47c9c9b03f7785f20921a527dd19
newlearner2020-code <69915518+newlearner2020-code@users.noreply.github.com>
2022-07-15 11:40:00 +0530

    Add files via upload
    0
    01
    010
    0101
    01010
    010101
    0101010
    01010101
    010101010
    
    print the upper pattern using this porogram:-

---
+++ pattern_with_numbers.cpp
@@ -0,0 +1,35 @@
+//pattern prinintg using numbers only 0 and 1:-
+#include<iostream>
+using namespace std;
+int i,j,n=10;
+int a=0,b=1;
+int main()
+{
+	for(i=0;i<n;i++)
+	{
+		if(i%2==0)
+		{
+			a=0;
+				b=1;
+		}
+		else
+		{
+				b=0;
+				a=1;
+		}
+		for(j=i;j>0;j--)
+		{  
+			if(j%2==0)
+			{
+				cout<<a;
+			}
+			else
+			{
+				cout<<b;
+			}
+		}
+		cout<<"\n";
+	}
+	
+	
+}
